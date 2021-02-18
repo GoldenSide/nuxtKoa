36079b214fa587544de7bdb153647b856bdfd9d9
GoldenSide <1244338192@qq.com>
2021-02-18 15:31:26 +0800

    content

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,34 @@
 <template>
-  <section class="container">
-    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
-      Universal Vue.js Application Framework
-    </h1>
-    <nuxt-link class="button" to="/about">
-      About page
-    </nuxt-link>
-  </section>
+  <div class="page-index">
+    <el-row>
+      <el-col :span="5">
+        <emenu />
+      </el-col>
+      <el-col :span="19">
+        <life />
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <artistic />
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
-<style scoped>
-.title
-{
-  margin: 50px 0;
-}
+<script>
+import Emenu from "@/components/index/menu.vue";
+import Life from "@/components/index/life.vue";
+import Artistic from "@/components/index/artistic.vue";
+export default {
+  components: {
+    Emenu,
+    Life,
+    Artistic,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/css/index/index.scss";
 </style>
