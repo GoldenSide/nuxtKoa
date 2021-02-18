e1fdf7dc83ee5a5d12f3136d559ebadc30002924
GoldenSide <1244338192@qq.com>
2021-02-18 18:45:58 +0800

    home 仓库

--- store/home.js
+++ store/home.js
@@ -1,1 +1,18 @@
-export const state = () => ({ hotPlace: [] });
+export const state = () => ({ hotPlace: [], menu: [] });
+
+export const mutations = {
+  setMenu(state, val) {
+    state.menu = val;
+  },
+  setHotPlace(state, val) {
+    state.hotPlace = val;
+  }
+};
+export const actions = {
+  setMenu: ({ commit }, menu) => {
+    commit("setMenu", menu);
+  },
+  setHotPlace: ({ commit }, hotPlace) => {
+    commit("setHotPlace", hotPlace);
+  }
+};
