3f20c4806f2ef095a37d1a87cfe75503af26c30f
GoldenSide <1244338192@qq.com>
2021-02-19 18:30:51 +0800

    服务配置

--- server/interface/users.js
+++ server/interface/users.js
@@ -1,5 +1,13 @@
 import Router from "koa-router";
+import Redis from "koa-redis";
+import nodemailer from "nodemailer";
+import User from "../dbs/models/users";
+import Passport from "./utils/passport";
+import Email from "../dbs/config";
+import axios from "./utils/axios";
 let router = new Router({ prefix: "/users" });
+
+// router.post
 
 router.get("/getUser", async ctx => {
   //   if (ctx.isAuthenticated()) {
