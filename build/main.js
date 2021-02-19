fc9f9dd38717317177f420c7da94c3e4617d9d08
GoldenSide <1244338192@qq.com>
2021-02-19 18:58:23 +0800

    注册登录

--- build/main.js
+++ build/main.js
@@ -4214,8 +4214,85 @@ __webpack_require__.r(__webpack_exports__);
 
 let router = new koa_router__WEBPACK_IMPORTED_MODULE_0___default.a({
   prefix: "/users"
-}); // router.post
+});
+let Store = new koa_redis__WEBPACK_IMPORTED_MODULE_1___default.a().client;
+router.post("/signup", async ctx => {
+  const {
+    username,
+    password,
+    email,
+    code
+  } = ctx.request.body;
 
+  if (code) {
+    const saveCode = await Store.hget(`nodemail:${username}`, "code");
+    const saveExpire = await Store.hget(`nodemail:${username}`, "expire");
+
+    if (code === saveCode) {
+      if (new Date().getTime() - saveExpire >= 0) {
+        ctx.body = {
+          code: -1,
+          msg: "验证码已过期，请重新尝试"
+        };
+      }
+
+      return false;
+    } else {
+      ctx.body = {
+        code: -1,
+        msg: "请填写正确的验证码"
+      };
+    }
+  } else {
+    ctx.body = {
+      code: -1,
+      msg: "验证码为必填项"
+    };
+  }
+
+  let user = await _dbs_models_users__WEBPACK_IMPORTED_MODULE_3__["default"].find({
+    username
+  });
+
+  if (user.length > 0) {
+    ctx.body = {
+      code: -1,
+      msg: "该用户名已被注册"
+    };
+    return false;
+  }
+
+  let nuser = await _dbs_models_users__WEBPACK_IMPORTED_MODULE_3__["default"].create({
+    username,
+    password,
+    email
+  });
+
+  if (nuser) {
+    let res = await _utils_axios__WEBPACK_IMPORTED_MODULE_6__["default"].post("/users/signin", {
+      username,
+      password
+    });
+
+    if (res.data && res.data.code == 0) {
+      ctx.body = {
+        code: 0,
+        msg: "注册成功",
+        user: res.data.user
+      };
+    } else {
+      ctx.body = {
+        code: -1,
+        msg: "登录失败"
+      };
+    }
+  } else {
+    ctx.body = {
+      code: -1,
+      msg: "注册失败"
+    };
+  }
+});
 router.get("/getUser", async ctx => {
   //   if (ctx.isAuthenticated()) {
   //     const { username, email } = ctx.session.passport.user;
